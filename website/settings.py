3ba67ef22a6fd132cfd6cfd182e99ceda812cf04
bbright3493 <44704708@qq.com>
2018-08-31 12:52:11 +0800

    修改跨域访问的问题

--- website/settings.py
+++ website/settings.py
@@ -60,12 +60,14 @@ INSTALLED_APPS = [
     'captcha',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 
 ]
 
 AUTH_USER_MODEL = "users.UserProfile"
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -76,6 +78,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'website.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
