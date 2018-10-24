6e740d374a72c80d35337993a3e1b8f9265961a2
bbright3493 <44704708@qq.com>
2018-10-24 16:39:12 +0800

    1 修改章节练习

--- website/urls.py
+++ website/urls.py
@@ -220,11 +220,12 @@ urlpatterns = [
     url(r'^admin/', xadmin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^api/', include(router.urls)),
+    url(r'^login/', obtain_jwt_token),  # jwt认证
     url(r'docs/', include_docs_urls(title='格子网塾api接口说明')),
 #    url(r'^index/', TemplateView.as_view(template_name="index.html"), name="index"),
     url(r'^', TemplateView.as_view(template_name="index.html"), name="index"),
     url(r'^results/', UserResultsView.as_view(), name='results'),  # 用户成绩api
-    url(r'^login/', obtain_jwt_token),  # jwt认证
+
     url(r'logout/', LogoutView.as_view(), name='logout'),  # 退出登录
     url(r'^ueditor/', include('DjangoUeditor.urls')),
     url(r'^media/(?P<path>.*)/$', serve, {'document_root': MEDIA_ROOT}),  # 资源文件加载地址
